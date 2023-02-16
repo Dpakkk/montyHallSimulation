20c9e5fc54ff385a971982e90ed02de1ece93085
Dpakkk <pokharelbikash981@gmail.com>
2023-02-16 16:47:31 -0500

    admin page - added

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route("/admin")
+def admin():
+    return "<h1>ADMIN</h1>"
+
+
 @app.route("/")
 def home():
     return "Hello from dssd!dshomepage!"
