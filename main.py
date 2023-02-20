ae65e1e2086a2b4149fafe5233b3590c73aa4324
Dpakkk <pokharelbikash981@gmail.com>
2023-02-20 15:12:29 -0500

    python code embedded in html file

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
   
+app.route("/")
+def default():
+    return "Home Page. Not an error!!" 
+
 @app.route("/<name>")
 def home(name):
-    return render_template("index.html", content = name, age = 45)
+    return render_template("index.html", content = ["sam","larry","robert"])
 
+  
 if __name__ == '__main__':
     app.run()
   
