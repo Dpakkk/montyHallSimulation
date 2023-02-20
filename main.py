1d2def84b82f40527973badfa36e99a89cce40b9
Dpakkk <pokharelbikash981@gmail.com>
2023-02-20 15:53:44 -0500

    botstrapping from one html file to another

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-  
-app.route("/")
-def default():
-    return "Home Page. Not an error!!" 
-
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content = ["sam","larry","robert"])
+ 
+@app.route("/")
+def home():
+    return render_template("index.html")
 
   
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
   
