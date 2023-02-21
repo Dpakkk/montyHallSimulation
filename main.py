e91486b4d11890bc57c5e389efc847fb5502c50c
Dpakkk <pokharelbikash981@gmail.com>
2023-02-21 15:53:04 -0500

    login files

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
  
+@app.route("/login", method = ["POST","GET"])
+def login():
+    return render_template()
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
 @app.route("/")
 def home():
     return render_template("index.html")
+    
 @app.route("/anotherone")
 def test():
     return render_template("test.html")
